29bd736703e89ca79fcac69b72360b4b9dbfadcb
EllaNillnor <elni06001@utb.vaxjo.se>
2024-10-14 09:54:33 +0200

    Math.pow metod

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.WriteLine("10 upphöjt till 7: "+Power(10,7));
+Console.WriteLine("2 upphöjt till 10: "+Power(2,10));
+
+double  Power(double  a, int b){
+  return Math.Pow(a,b);
+   
+
+}
